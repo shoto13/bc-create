8c6275e32e1dbc24ed590491cca353dfbb290b4e
rory <wilsonrc@pm.me>
2022-09-11 22:56:15 +0100

    initial pow function build

--- blockchain.py
+++ blockchain.py
@@ -18,6 +18,20 @@ class Blockchain:
             'proof': proof,
             'previous_hash': previous_hash,
             }
-
         self.chain.append(block)
         return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_function(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
